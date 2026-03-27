52dd639a301b43d0595601f1adafc56a5f949673
mohamedkhaledabdelfattah23 <mohamedkhaledabdelfattah23@gmail.com>
2026-03-27 22:43:17 +0200

    ADV01

---
+++ AdvancedC#01/MyStructClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvancedC_01
+{
+    internal class MyStructClass<T> where T : struct
+    {
+
+
+
+    }
+}
---
+++ AdvancedC#01/Pair.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvancedC_01
+{
+    internal class Pair<TKey , TValue>
+    {
+
+        public TKey Key { get; set; }
+        public TValue Value { get; set; }
+
+    }
+}
